7aab344d1a6c6ce29fac338d344140f81b30063b
VivM <vivianmwilaria@gmail.com>
2023-12-05 00:33:18 +0300

    third commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,9 +1,31 @@
+def add(a, b):
+    z= a + b
+    return z
 a = 1
 b = 2
-print (a+b)
-x = 98
-y= 0
-print(x+y)
-z= 100
-k= -2
-print(z+k)
+result= add (a,b)
+print ('a = {} b = {} a + b ={}'.format (a, b, result))
+
+def add(a, b):
+    z= a + b
+    return z
+a = 98
+b = 0
+result= add (a,b)
+print ('a = {} b = {} a + b ={}'.format (a, b, result))
+
+def add(a, b):
+    z= a + b
+    return z
+a = -100
+b = -2
+result= add (a,b)
+print ('a = {} b = {} a + b ={}'.format (a, b, result))
+
+def add(a, b):
+    z= a + b
+    return z
+a = 0
+b = 0
+result= add (a,b)
+print ('a = {} b = {} a + b ={}'.format (a, b, result))
