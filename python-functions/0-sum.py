f3f8b4b2bc45abb87b0b6ac01c2ba9e54b2a8f42
VivM <vivianmwilaria@gmail.com>
2023-12-05 19:11:36 +0300

    3rd commir

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,31 +1,12 @@
-def add(a, b):
-    z= a + b
-    return z
-a = 1
-b = 2
-result= add (a,b)
-print ('a = {} b = {} a + b ={}'.format (a, b, result))
-
-def add(a, b):
-    z= a + b
-    return z
-a = 98
-b = 0
-result= add (a,b)
-print ('a = {} b = {} a + b ={}'.format (a, b, result))
-
-def add(a, b):
-    z= a + b
-    return z
-a = -100
-b = -2
-result= add (a,b)
-print ('a = {} b = {} a + b ={}'.format (a, b, result))
-
-def add(a, b):
-    z= a + b
-    return z
-a = 0
-b = 0
-result= add (a,b)
-print ('a = {} b = {} a + b ={}'.format (a, b, result))
+def addNumber():
+    print (1 + 2)
+addNumber()
+def addNumber():
+    print(98 + 0)
+addNumber()
+def addNumber():
+    print(100+ -2)
+addNumber()
+def addNumber():
+    print(0 + 0)
+addNumber()
