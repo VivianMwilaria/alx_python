876c47ea1831bebfa4838abb98bd68d23ab75de0
VivM <vivianmwilaria@gmail.com>
2023-12-06 10:09:22 +0300

    commit

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,16 +1,3 @@
 def pow (a, b):
     result = a ** b
     return result
-#example
-base = 2
-exponential = 2
-result = pow(base, exponential)
-print(result)
-
-  #example 2
-base = 98
-exponential = 2
-result = pow(base, exponential)
-print(result)
-
-
