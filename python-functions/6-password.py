c916cdfd56c3847e13682e02eccfd04e1c2bac10
VivM <vivianmwilaria@gmail.com>
2023-12-06 11:00:54 +0300

    first commit

---
+++ python-functions/6-password.py
@@ -0,0 +1,23 @@
+def validate_password(password):
+    # Check length
+    if len(password) < 8:
+        return False
+
+    # Check for at least one uppercase letter, one lowercase letter, and one digit
+    has_uppercase = any(char.isupper() for char in password)
+    has_lowercase = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+
+    if not (has_uppercase and has_lowercase and has_digit):
+        return False
+
+    # Check for spaces
+    if ' ' in password:
+        return False
+
+    # If all checks pass, return True
+    return True
+
+
+
+
