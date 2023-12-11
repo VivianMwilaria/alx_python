da17071106d048e8d815766d4e7470de66e0dd38
VivM <vivianmwilaria@gmail.com>
2023-12-11 22:10:39 +0300

    3rd commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,9 @@
 # File: add_0.py
 # Contains the function def add(a, b)
 
-def add(a,b):
-    a = 1
-    b = 2
-sum_result = add(a, b)
-# Importing the add function from add_0.py
 from add_0 import add
-
+a = 1
+b = 2
 
 # Printing the result using string formatting
 print("{} + {} = {}".format(a, b, add(a, b)))
