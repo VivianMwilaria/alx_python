11da091789428c82b7ab573323f14c3fd59504cc
VivM <vivianmwilaria@gmail.com>
2023-12-12 08:01:33 +0300

    6th commit

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,3 +1,11 @@
 def add(a,b):
-    result= a + b
-    return result
+     """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+return (a + b)
