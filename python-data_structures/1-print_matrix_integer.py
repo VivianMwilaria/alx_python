c406732f602b7e9a841e35c5227ea0a6853d6466
VivM <vivianmwilaria@gmail.com>
2024-01-01 08:40:09 +0300

    first commit

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,8 @@
+def print_matrix_integer(matrix=[[]]):
+    # Iterate through each row in the matrix
+    for row in matrix:
+        # Iterate through each element in the row
+        for element in row:
+            # Use str.format() to print integers without casting to strings
+            print("{:d}".format(element), end=" ")
+        print()  # Move to the next line after printing each row
