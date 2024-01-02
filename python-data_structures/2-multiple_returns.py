9d34b154631bb7bff5eb696b7947bc63c37665fa
VivM <vivianmwilaria@gmail.com>
2024-01-02 14:56:44 +0300

    first commit

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,9 @@
+def multiple_returns(sentence):
+    # Check if the sentence is empty
+    if len(sentence) == 0:
+        # If empty, return a tuple with length None and first character None
+        return (None, None)
+    else:
+        # If not empty, return a tuple with length of the string and its first character
+        return (len(sentence), sentence[0])
+
