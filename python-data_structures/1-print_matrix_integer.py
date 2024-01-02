e53db33dac22087a6b59be015086b89abffd0a14
VivM <vivianmwilaria@gmail.com>
2024-01-02 15:57:46 +0300

    3rd commit

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,7 +2,12 @@ def print_matrix_integer(matrix=[[]]):
     # Iterate through each row in the matrix
     for row in matrix:
         # Iterate through each element in the row
-        for element in row:
+        for index, element in enumerate(row):
             # Use str.format() to print integers without casting to strings
-            print("{:d}".format(element), end=" ")
-    print()  # Move to the next line after printing each row
+            if index == len(row) - 1:
+                # If it's the last element in the row, print without trailing space
+                print("{:d}".format(element))
+            else:
+                # Print the element with a trailing space
+                print("{:d}".format(element), end=" ")
+
