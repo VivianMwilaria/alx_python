0f1fceaa94edf280ceafef16cbd38d119a53dad3
VivM <vivianmwilaria@gmail.com>
2024-01-14 21:48:16 +0300

    2nd commit

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,15 +1,11 @@
 def update_dictionary(a_dictionary, key, value):
-    # Update the value if the key exists, or add a new key-value pair if it doesn't
-    a_dictionary[key] = value
+    # Check if the key already exists in the dictionary
+    if key in a_dictionary:
+        # If the key exists, replace the existing value
+        a_dictionary[key] = value
+    else:
+        # If the key doesn't exist, add a new key-value pair
+        a_dictionary[key] = value
+
     return a_dictionary
 
-# Example usage:
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-
-# Updating existing key 'b'
-update_dictionary(my_dict, 'b', 10)
-print(my_dict)  # Output: {'a': 1, 'b': 10, 'c': 3}
-
-# Adding a new key 'd'
-update_dictionary(my_dict, 'd', 4)
-print(my_dict)  # Output: {'a': 1, 'b': 10, 'c': 3, 'd': 4}
