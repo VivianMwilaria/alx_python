aa1fc96748b24855af7c7e6fed40dd56fefb5ace
VivM <vivianmwilaria@gmail.com>
2024-01-29 10:39:25 +0300

    1st commit

---
+++  python-classes/0-square.py
@@ -0,0 +1,17 @@
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def perimeter(self):
+        return 4 * self.__size
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, new_size):
+        self.__size = new_size
+
+
