e624c5f64b170b057052634277e54cc8f2c54ee9
VivM <vivianmwilaria@gmail.com>
2024-01-31 17:38:20 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,12 @@
-class Square:
-    """
+"""
     Represents a square with a size attribute.
 
     Attributes:
         __size (int): The size of the square.
-    """
+"""
+
+class Square:
+    pass
 
     def __init__(self, size):
         self.__size = size
