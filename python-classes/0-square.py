df173499652c80f3ec3f9179a26cdfd883d77eec
VivM <vivianmwilaria@gmail.com>
2024-01-31 17:29:21 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,37 +7,4 @@ class Square:
     """
 
     def __init__(self, size):
-        """
-        Initializes a Square object with a given size.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
-
-    def get_size(self):
-        """
-        Gets the size of the square.
-
-        Returns:
-            int: The size of the square.
-        """
-        return self.__size
-
-    def set_size(self, size):
-        """
-        Sets the size of the square.
-
-        Args:
-            size (int): The size of the square.
-        """
-        self.__size = size
-
-    def area(self):
-        """
-        Calculates the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
