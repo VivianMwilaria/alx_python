f5bbbf448238cf86aa50066255ac8319ebd1146c
VivM <vivianmwilaria@gmail.com>
2024-02-01 10:23:06 +0300

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,6 @@
+#!/usr/bin/python3
 class Square:
-  """Defines a square"""
-def __init__(self, size=0, position=(0, 0)):
+    """Defines a square"""
+    def __init__(self, size):
         """Initialises the data"""
-        self.size = size
-        self.position = position
+        self.__size = size
