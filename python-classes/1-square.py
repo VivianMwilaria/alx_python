a63b8878f14fb9ca1e1f8c23417e40cc36153d9a
VivM <vivianmwilaria@gmail.com>
2024-02-02 00:19:11 +0300

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,17 @@
 class Square:
     """class that defines square"""
+
+    """The __init__ method is run as soon as the object is instatiated
+    Args : 
+    Size: The sixe of the square. I must be a positive integer.
+    Attribute: 
+    __size: rivaye instance attribute that is the size of the square
+    """
     def __init__(self, size=0):
-        
-        if not isinstance(size, int):
+        self.__size = size
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
