5144e1f818d8e6f8653a1528fccea36a15166d2d
VivM <vivianmwilaria@gmail.com>
2024-02-01 10:27:07 +0300

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python3
 class Square:
-    """Defines a square"""
-    def __init__(self, size):
-        """Initialises the data"""
+    """defines square"""
+    def __init__(self, size=0):
+        """initalizes size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
