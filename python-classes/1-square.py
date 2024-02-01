1c25edd3160e40877a372e6aff5eed3775ac1df9
VivM <vivianmwilaria@gmail.com>
2024-02-01 10:15:48 +0300

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,6 @@
 class Square:
+    """Defines a square"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
+       """initializes the data """
+       self.__size= size
 
