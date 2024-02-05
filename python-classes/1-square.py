a3834d3785793671062eaee9e624e26b6632a599
VivM <vivianmwilaria@gmail.com>
2024-02-05 10:16:43 +0300

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,17 +1,17 @@
+"""
+Module 1-square
+Define class Square with private attribute size
+"""
+
+
 class Square:
-    """class that defines square"""
+    """Represent s squqre."""
 
-    """The __init__ method is run as soon as the object is instatiated
-    Args : 
-    Size: The sixe of the square. I must be a positive integer.
-    Attribute: 
-    __size: rivaye instance attribute that is the size of the square
-    """
-    def __init__(self, size=0):
-        self.__size = size
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+    def __init__(self, size):
+        """
+        Initializes square
 
+        Attributes:
+            size: size of a square
+        """
+        self.__size = size
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,16 @@
-class Square:
-    """Defines a square"""
-    def __init__(self, size=0):
-        """Initialises the data"""
-        self.__size = size
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+"""Define a class Square."""
 
-    def area(self):
-        """Returns current square area"""
-        return self.__size**2
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
