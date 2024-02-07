b6849b1625f9b57e20ebe02655502d4cbe090934
VivM <vivianmwilaria@gmail.com>
2024-02-07 11:51:39 +0300

    commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,15 @@
-"""Define a class Square."""
-
+"""
+Module 1-square
+Define class Square with private attribute size
+"""
 
 class Square:
-    """Represent a square."""
-
     def __init__(self, size=0):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
