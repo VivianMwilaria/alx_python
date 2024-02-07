dcf30206255640318288f3c1062700c9340963eb
VivM <vivianmwilaria@gmail.com>
2024-02-07 11:48:32 +0300

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -15,3 +15,10 @@ class Square:
             size: size of a square
         """
         self.__size = size
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
